46142d8c6fddfaae2a5d0517a293973a5a48bb58
dingmark <dingmark@163.com>
2021-05-07 16:34:08 +0800

    登录页面提示解决。添加数据库用户。放弃formjs提交

--- src/main/resources/static/js/newchose.js
+++ src/main/resources/static/js/newchose.js
@@ -9,119 +9,24 @@ $(document).ready(function(){
         // f1(this,f2);
     });
     $("#selectbuy").on('change',function (e) {
-        switch (this.value)
-        {
-            case'ok':
-                $(".warning").hide();
-                //$(".buyok").parent().show();
-                $("tr").find(".buyok").parent().show();
-                break;
-            case'hb':
-                $(".warning").hide();
-                //$(".buyhb").parent().show();
-                $("tr").find(".buyhb").parent().show();
-                break;
-            case 'all':
-                $(".warning").show();
-                break;
-        }
+        buyselect(this.value);
+        
     });
     $("#selectsell").on('change',function (e) {
-        switch (this.value)
-        {
-            case'ok':
-                // $(".warning").hide();
-                //$(".buyok").parent().show();
-                $('tr:visible').find(".sellhb").parent().hide();
-                $('tr:visible').find(".sellbn").parent().hide();
-                $('tr:visible').find(".sellbt").parent().hide();
-                // $('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                $('tr:visible').find(".sellzb").parent().hide();
-                $('tr:visible').find(".sellbs").parent().hide();
-                $('tr:visible').find(".sellkb").parent().hide();
-                //$("tr").find(".sellok").parent().show();
-                break;
-            case'hb':
-                //$('tr:visible').find(".sellhb").parent.hide();
-                $('tr:visible').find(".sellbn").parent().hide();
-                $('tr:visible').find(".sellbt").parent().hide();
-                $('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                $('tr:visible').find(".sellzb").parent().hide();
-                $('tr:visible').find(".sellbs").parent().hide();
-                $('tr:visible').find(".sellkb").parent().hide();
-                break;
-            case 'bn':
-                $('tr:visible').find(".sellhb").parent().hide();
-               // $('tr:visible').find(".sellbn").parent().hide();
-                $('tr:visible').find(".sellbt").parent().hide();
-                $('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                $('tr:visible').find(".sellzb").parent().hide();
-                $('tr:visible').find(".sellbs").parent().hide();
-                $('tr:visible').find(".sellkb").parent().hide();
-                break;
-            case 'bt':
-                $('tr:visible').find(".sellhb").parent().hide();
-                $('tr:visible').find(".sellbn").parent().hide();
-                //$('tr:visible').find(".sellbt").parent().hide();
-                $('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                $('tr:visible').find(".sellzb").parent().hide();
-                $('tr:visible').find(".sellbs").parent().hide();
-                $('tr:visible').find(".sellkb").parent().hide();
-                break;
-            case 'bs':
-                $('tr:visible').find(".sellhb").parent().hide();
-                $('tr:visible').find(".sellbn").parent().hide();
-                $('tr:visible').find(".sellbt").parent().hide();
-                $('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                $('tr:visible').find(".sellzb").parent().hide();
-                //$('tr:visible').find(".sellbs").parent().hide();
-                $('tr:visible').find(".sellkb").parent().hide();
-                break;
-            case 'kb':
-                $('tr:visible').find(".sellhb").parent().hide();
-                $('tr:visible').find(".sellbn").parent().hide();
-                $('tr:visible').find(".sellbt").parent().hide();
-                $('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                $('tr:visible').find(".sellzb").parent().hide();
-                $('tr:visible').find(".sellbs").parent().hide();
-                //$('tr:visible').find(".sellkb").parent().hide();
-                break;
-            case 'zb':
-                $('tr:visible').find(".sellhb").parent().hide();
-                $('tr:visible').find(".sellbn").parent().hide();
-                $('tr:visible').find(".sellbt").parent().hide();
-                $('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                //$('tr:visible').find(".sellzb").parent().hide();
-                $('tr:visible').find(".sellbs").parent().hide();
-                $('tr:visible').find(".sellkb").parent().hide();
-                break;
-            case 'ok':
-                $('tr:visible').find(".sellhb").parent().hide();
-                $('tr:visible').find(".sellbn").parent().hide();
-                $('tr:visible').find(".sellbt").parent().hide();
-                //$('tr:visible').find(".sellok").parent().hide();
-                $('tr:visible').find(".sellmc").parent().hide();
-                $('tr:visible').find(".sellzb").parent().hide();
-                $('tr:visible').find(".sellbs").parent().hide();
-                $('tr:visible').find(".sellkb").parent().hide();
-                break;
-            case 'all':
-                $(".warning").show();
-                break;
-        }
+        sellselect(this.value);
     });
 
     $("#bz").on('change',function (e) {
         //alert(1111);
+        if($.trim(this.value)!='')
+        {
         $('tr :visible').find(".btn-warning").not("."+this.value)
             .parent().parent().hide();
+        }
+        else
+        {
+            $(".warning").show();
+        }
         //$('tr:visible').find("."+this.value).parent().show();
        // $("tr").find(".sellhb").parent().show();
     });
@@ -132,4 +37,185 @@ function f1(aa,callback) {
 }
 function f2() {
     $("[data-toggle='popover']").popover();
+}
+function buyselect(buy) {
+    switch (buy)
+    {
+        case'ok':
+            $('tr:visible').find(".buyhb").parent().hide();
+            $('tr:visible').find(".buybn").parent().hide();
+            $('tr:visible').find(".buybt").parent().hide();
+            // $('tr:visible').find(".buyok").parent().hide();
+            $('tr:visible').find(".buymc").parent().hide();
+            $('tr:visible').find(".buyzb").parent().hide();
+            $('tr:visible').find(".buybs").parent().hide();
+            $('tr:visible').find(".buykb").parent().hide();
+            //$("tr").find(".buyok").parent().show();
+            break;
+        case'hb':
+            //$('tr:visible').find(".buyhb").parent.hide();
+            $('tr:visible').find(".buybn").parent().hide();
+            $('tr:visible').find(".buybt").parent().hide();
+            $('tr:visible').find(".buyok").parent().hide();
+            $('tr:visible').find(".buymc").parent().hide();
+            $('tr:visible').find(".buyzb").parent().hide();
+            $('tr:visible').find(".buybs").parent().hide();
+            $('tr:visible').find(".buykb").parent().hide();
+            break;
+        case 'bn':
+            $('tr:visible').find(".buyhb").parent().hide();
+            // $('tr:visible').find(".buybn").parent().hide();
+            $('tr:visible').find(".buybt").parent().hide();
+            $('tr:visible').find(".buyok").parent().hide();
+            $('tr:visible').find(".buymc").parent().hide();
+            $('tr:visible').find(".buyzb").parent().hide();
+            $('tr:visible').find(".buybs").parent().hide();
+            $('tr:visible').find(".buykb").parent().hide();
+            break;
+        case 'bt':
+            $('tr:visible').find(".buyhb").parent().hide();
+            $('tr:visible').find(".buybn").parent().hide();
+            //$('tr:visible').find(".buybt").parent().hide();
+            $('tr:visible').find(".buyok").parent().hide();
+            $('tr:visible').find(".buymc").parent().hide();
+            $('tr:visible').find(".buyzb").parent().hide();
+            $('tr:visible').find(".buybs").parent().hide();
+            $('tr:visible').find(".buykb").parent().hide();
+            break;
+        case 'bs':
+            $('tr:visible').find(".buyhb").parent().hide();
+            $('tr:visible').find(".buybn").parent().hide();
+            $('tr:visible').find(".buybt").parent().hide();
+            $('tr:visible').find(".buyok").parent().hide();
+            $('tr:visible').find(".buymc").parent().hide();
+            $('tr:visible').find(".buyzb").parent().hide();
+            //$('tr:visible').find(".buybs").parent().hide();
+            $('tr:visible').find(".buykb").parent().hide();
+            break;
+        case 'kb':
+            $('tr:visible').find(".buyhb").parent().hide();
+            $('tr:visible').find(".buybn").parent().hide();
+            $('tr:visible').find(".buybt").parent().hide();
+            $('tr:visible').find(".buyok").parent().hide();
+            $('tr:visible').find(".buymc").parent().hide();
+            $('tr:visible').find(".buyzb").parent().hide();
+            $('tr:visible').find(".buybs").parent().hide();
+            //$('tr:visible').find(".buykb").parent().hide();
+            break;
+        case 'zb':
+            $('tr:visible').find(".buyhb").parent().hide();
+            $('tr:visible').find(".buybn").parent().hide();
+            $('tr:visible').find(".buybt").parent().hide();
+            $('tr:visible').find(".buyok").parent().hide();
+            $('tr:visible').find(".buymc").parent().hide();
+            //$('tr:visible').find(".buyzb").parent().hide();
+            $('tr:visible').find(".buybs").parent().hide();
+            $('tr:visible').find(".buykb").parent().hide();
+            break;
+        case 'mc':
+            $('tr:visible').find(".buyhb").parent().hide();
+            $('tr:visible').find(".buybn").parent().hide();
+            $('tr:visible').find(".buybt").parent().hide();
+            $('tr:visible').find(".buyok").parent().hide();
+            // $('tr:visible').find(".buymc").parent().hide();
+            $('tr:visible').find(".buyzb").parent().hide();
+            $('tr:visible').find(".buybs").parent().hide();
+            $('tr:visible').find(".buykb").parent().hide();
+            break;
+        case 'all':
+            $(".warning").show();
+            break;
+    }
+}
+function sellselect(sell) {
+    switch (sell)
+    {
+        case'ok':
+            // $(".warning").hide();
+            //$(".buyok").parent().show();
+            $('tr:visible').find(".sellhb").parent().hide();
+            $('tr:visible').find(".sellbn").parent().hide();
+            $('tr:visible').find(".sellbt").parent().hide();
+            // $('tr:visible').find(".sellok").parent().hide();
+            $('tr:visible').find(".sellmc").parent().hide();
+            $('tr:visible').find(".sellzb").parent().hide();
+            $('tr:visible').find(".sellbs").parent().hide();
+            $('tr:visible').find(".sellkb").parent().hide();
+            //$("tr").find(".sellok").parent().show();
+            break;
+        case'hb':
+            //$('tr:visible').find(".sellhb").parent.hide();
+            $('tr:visible').find(".sellbn").parent().hide();
+            $('tr:visible').find(".sellbt").parent().hide();
+            $('tr:visible').find(".sellok").parent().hide();
+            $('tr:visible').find(".sellmc").parent().hide();
+            $('tr:visible').find(".sellzb").parent().hide();
+            $('tr:visible').find(".sellbs").parent().hide();
+            $('tr:visible').find(".sellkb").parent().hide();
+            break;
+        case 'bn':
+            $('tr:visible').find(".sellhb").parent().hide();
+            // $('tr:visible').find(".sellbn").parent().hide();
+            $('tr:visible').find(".sellbt").parent().hide();
+            $('tr:visible').find(".sellok").parent().hide();
+            $('tr:visible').find(".sellmc").parent().hide();
+            $('tr:visible').find(".sellzb").parent().hide();
+            $('tr:visible').find(".sellbs").parent().hide();
+            $('tr:visible').find(".sellkb").parent().hide();
+            break;
+        case 'bt':
+            $('tr:visible').find(".sellhb").parent().hide();
+            $('tr:visible').find(".sellbn").parent().hide();
+            //$('tr:visible').find(".sellbt").parent().hide();
+            $('tr:visible').find(".sellok").parent().hide();
+            $('tr:visible').find(".sellmc").parent().hide();
+            $('tr:visible').find(".sellzb").parent().hide();
+            $('tr:visible').find(".sellbs").parent().hide();
+            $('tr:visible').find(".sellkb").parent().hide();
+            break;
+        case 'bs':
+            $('tr:visible').find(".sellhb").parent().hide();
+            $('tr:visible').find(".sellbn").parent().hide();
+            $('tr:visible').find(".sellbt").parent().hide();
+            $('tr:visible').find(".sellok").parent().hide();
+            $('tr:visible').find(".sellmc").parent().hide();
+            $('tr:visible').find(".sellzb").parent().hide();
+            //$('tr:visible').find(".sellbs").parent().hide();
+            $('tr:visible').find(".sellkb").parent().hide();
+            break;
+        case 'kb':
+            $('tr:visible').find(".sellhb").parent().hide();
+            $('tr:visible').find(".sellbn").parent().hide();
+            $('tr:visible').find(".sellbt").parent().hide();
+            $('tr:visible').find(".sellok").parent().hide();
+            $('tr:visible').find(".sellmc").parent().hide();
+            $('tr:visible').find(".sellzb").parent().hide();
+            $('tr:visible').find(".sellbs").parent().hide();
+            //$('tr:visible').find(".sellkb").parent().hide();
+            break;
+        case 'zb':
+            $('tr:visible').find(".sellhb").parent().hide();
+            $('tr:visible').find(".sellbn").parent().hide();
+            $('tr:visible').find(".sellbt").parent().hide();
+            $('tr:visible').find(".sellok").parent().hide();
+            $('tr:visible').find(".sellmc").parent().hide();
+            //$('tr:visible').find(".sellzb").parent().hide();
+            $('tr:visible').find(".sellbs").parent().hide();
+            $('tr:visible').find(".sellkb").parent().hide();
+            break;
+        case 'mc':
+            $('tr:visible').find(".sellhb").parent().hide();
+            $('tr:visible').find(".sellbn").parent().hide();
+            $('tr:visible').find(".sellbt").parent().hide();
+            $('tr:visible').find(".sellok").parent().hide();
+            // $('tr:visible').find(".sellmc").parent().hide();
+            $('tr:visible').find(".sellzb").parent().hide();
+            $('tr:visible').find(".sellbs").parent().hide();
+            $('tr:visible').find(".sellkb").parent().hide();
+            break;
+        case 'all':
+            $(".warning").show();
+            break;
+    }
+    
 }
